eafbc6dda68f37577d0ca0e128f4d9e3aa2eb56e
Tavo <gavc1@DESKTOP-IVNC8A5>
2017-11-12 21:05:54 -0600

    Conexion

---
+++ ProyectoBD2/Datos/Conexion.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Conexion
+    {
+        SqlConnection objConnection = new SqlConnection("Server=tcp:proyectodb02.database.windows.net,1433;Initial Catalog=ProyectoBD02;Persist Security Info=False;User ID=administrador;Password=Admin12.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                objConnection.Open();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                if (objConnection.State == ConnectionState.Closed)
+                {
+                    return true;
+                }
+                objConnection.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                objConnection.Close();
+            }
+        }
+
+        public DataTable ExecuteQuery(SqlCommand oSQLC)
+        {
+            try
+            {
+                DataTable oDT = new DataTable();
+                SqlDataAdapter oSQLDA = new SqlDataAdapter(oSQLC);
+                oSQLC.Connection = objConnection;
+                if (OpenConnection())
+                {
+                    oSQLDA.Fill(oDT);
+                }
+                CloseConnection();
+                return oDT;
+            }
+            catch (SqlException e)
+            {
+                throw e;
+            }
+        }
+    }
+}
